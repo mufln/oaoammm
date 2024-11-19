57398227be9a11084089def182dcfd09578a9b43
leshenya <petrovako60021041@gmail.com>
2024-11-19 10:25:34 +0300

    Чуть-чуть роутер для расписания

--- Routers/TimeTable.cs
+++ Routers/TimeTable.cs
@@ -1,7 +1,85 @@
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using hihihiha.Services;
+using hihihiha.Models; 
+using hihihiha.Context;
 
 namespace hihihiha.Routers;
 
-public class TimeTable
+[ApiController]
+[Route("timetable")]
+public class TimeTable : ControllerBase
 {
+    private readonly ApplicationContext _context;
+
+    public TimeTable(ApplicationContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public ActionResult<List<TimeTable>> GetAllTimeTables()
+    {
+        var timeTables = TimeTableProvider.GetAllTimeTables(_context);
+        return Ok(timeTables);
+    }
+    
+    [HttpGet("{id}")]
+    public ActionResult<TimeTable> GetTimeTableById(int id)
+    {
+        var timeTable = TimeTableProvider.GetTimeTableById(_context, id);
+        if (timeTable == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(timeTable);
+    }
+    
+    [HttpPost]
+    public ActionResult CreateTimeTable([FromBody] Models.TimeTable timeTable)
+    {
+        if (timeTable == null)
+        {
+            return BadRequest("TimeTable cannot be null.");
+        }
+
+        try
+        {
+            TimeTableProvider.CreateTimeTable(_context, timeTable);
+            return Created("/api/timetable",  timeTable);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Internal server error");
+        }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdateTimeTable([FromBody] Models.TimeTable timeTable, int id)
+    {
+        try
+        {
+            timeTable.Id = id;
+            TimeTableProvider.UpdateTimeTable(_context, timeTable);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Internal server error");
+        }
+    }
+    
+    [HttpDelete("{id}")]
+    public ActionResult DeleteTimeTable(int id)
+    {
+        try
+        {
+            TimeTableProvider.DeleteTimeTable(_context, id);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Internal server error");
+        }
+    }
 }
